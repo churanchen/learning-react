15684c55ef94fc4d9bbfd6cb767d8c01b98349bc
Churan Chen <churan.chen@gmail.com>
2018-05-07 11:36:28 -0400

    Can edit timers

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -70,6 +70,27 @@ class EditableTimer extends React.Component {
     editFormOpen: false,
   };
 
+  handleEditClick = () => {
+    this.openForm();
+  };
+
+  handleFormClose = () => {
+    this.closeForm();
+  };
+
+  handleSubmit = (timer) => {
+    this.props.onFormSubmit(timer);
+    this.closeForm();
+  };
+
+  closeForm = () => {
+    this.setState({ editFormOpen: false });
+  };
+
+  openForm = () => {
+    this.setState({ editFormOpen: true });
+  };
+
 	render() {
 		if (this.state.editFormOpen) {
 			return (
@@ -77,6 +98,8 @@ class EditableTimer extends React.Component {
           id={this.props.id}
 					title={this.props.title}
 					project={this.props.project}
+          onFormSubmit={this.handleSubmit}
+          onFormClose={this.handleFormClose}
 				/>
 			);
 		} else {
@@ -87,6 +110,7 @@ class EditableTimer extends React.Component {
 					project={this.props.project}
 					elapsed={this.props.elapsed}
 					runningSince={this.props.runningSince}
+          onEditClick={this.handleEditClick}
 				/>
 			);
 		}
@@ -217,7 +241,10 @@ class Timer extends React.Component {
             </h2>
           </div>
           <div className='extra content'>
-            <span className='right floated edit icon'>
+            <span
+              className='right floated edit icon'
+              onClick={this.props.onEditClick}
+            >
               <i className='edit icon' />
             </span>
             <span className='right floated trash icon'>
